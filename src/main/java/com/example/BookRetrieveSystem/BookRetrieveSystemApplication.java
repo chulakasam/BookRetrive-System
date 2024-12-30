6c36cb1dd14f09c5fdeebdabeb3599dae2391ee4
chulakasam <sampathchulaka33872@gmail.com>
2024-12-30 21:51:00 +0530

    spring initializer

---
+++ src/main/java/com/example/BookRetrieveSystem/BookRetrieveSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookRetrieveSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRetrieveSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRetrieveSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookRetrieveSystem/BookRetrieveSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookRetrieveSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRetrieveSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
