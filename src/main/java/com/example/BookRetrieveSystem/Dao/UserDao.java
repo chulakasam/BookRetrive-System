2aee052548af8ff710eafdd2ecad8b4811f80f8d
chulakasam <sampathchulaka33872@gmail.com>
2024-12-31 14:21:38 +0530

    add userDao,user controller

--- src/main/java/com/example/BookRetrieveSystem/Dao/UserDao.java
+++ src/main/java/com/example/BookRetrieveSystem/Dao/UserDao.java
@@ -1,4 +1,12 @@
 package com.example.BookRetrieveSystem.Dao;
 
-public interface UserDao {
+import com.example.BookRetrieveSystem.Entity.impl.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDao extends JpaRepository<UserEntity,String> {
+    Optional<UserEntity> findByEmail(String email);
 }
--- src/main/java/com/example/BookRetrieveSystem/controller/UserController.java
+++ src/main/java/com/example/BookRetrieveSystem/controller/UserController.java
@@ -1,5 +1,32 @@
 package com.example.BookRetrieveSystem.controller;
 
+import com.example.BookRetrieveSystem.Secure.Response.JWTAuthResponse;
+import com.example.BookRetrieveSystem.Secure.Security.SignIn;
+import com.example.BookRetrieveSystem.Secure.Security.SignUp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
 public class UserController {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<JWTAuthResponse> saveUser(@RequestBody SignUp signUp) {
+        return ResponseEntity.ok(authenticationService.signUp(signUp));
+    }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<JWTAuthResponse> signIn(@RequestBody SignIn signIn) {
+        return ResponseEntity.ok(authenticationService.signIn(signIn));
+    }
+
+
+
 
 }
